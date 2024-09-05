18b21b1a77e8116417f04c0b22514e033429aa5e
Wonseok <dnjstjr970302@gmail.com>
2024-09-05 15:09:09 +0900

    Segmentation loading status management with Suspense & Streamed Response

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,14 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -20,8 +25,14 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
 }
+
+//이렇게 분리 함으로써 우리는 데이터 가져오는 부분을 분리된 컴포넌트로 아웃소싱함
